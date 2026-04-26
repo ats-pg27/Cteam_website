97609420c3e30a4ec7ea97874592c13cca67e1e1
Brist-27 <ats.spc17@gmail.com>
2026-04-26 16:52:34 +0900

    correct

--- frontend/React-App/src/features/Home.tsx
+++ frontend/React-App/src/features/Home.tsx
@@ -1,8 +1,14 @@
 
-const Home = () =>{
-    
-    return(
-        <h1>Home</h1>
+const Home = () => {
+
+    return (
+        <>
+            <h1>Home</h1>
+            <p>
+                文章文章文章文章文章文章文章文章文章文章文章文章文章文章文章文章文章文章
+            </p>
+        </>
+
     )
 }
 export default Home;
